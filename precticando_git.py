91901c2a28d2d6cf047cdca49619b690225db615
Byron <byronzurit_1990@hotmail.com>
2026-04-27 02:03:10 -0500

    actualizando

--- precticando_git.py
+++ precticando_git.py
@@ -1,1 +1,15 @@
-print("fola mundo")
+# condicional for 
+
+numeros = [4, 8, 15 ,23, 42]
+buscado = input("Ingrese el numero a buscar: ")
+
+for numero in numeros:
+    if numero == buscado:
+        
+        print(f"!Encontrando! El {buscado} esta en la lista ")
+        break
+    print(f"Revisando {numero}.... no es ")
+
+else:
+    print(f"El {buscado} no esta en la lista")
+
